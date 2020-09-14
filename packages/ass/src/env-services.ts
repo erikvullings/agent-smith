2f12556894d01055e39005151d5f97585b8cc4d2
arnoud <damylen@gmail.com>
2020-09-14 13:54:19 +0200

    ass: updated simulation to work without car and adjusted speed

--- packages/ass/src/env-services.ts
+++ packages/ass/src/env-services.ts
@@ -71,7 +71,7 @@ const createAgenda = (agent: IAgent, _services: IEnvServices) => {
   const { _day: day } = agent;
 
   agent.agenda = [
-    { name: 'Go to work', options: { startTime: simTime(day, randomInRange(6, 9), randomInRange(0, 59)) } },
+    { name: 'Go to work', options: { startTime: simTime(day, randomInRange(0, 4), randomInRange(0, 3)) } },
     { name: 'Work', options: { duration: hours(3, 5) } },
     { name: 'Have lunch' },
     { name: 'Work', options: { duration: hours(3, 5) } },
