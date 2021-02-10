7ea049def9daeb25adbc0b570b3931f57d31cd1a
erikvullings <erik.vullings@gmail.com>
2021-02-10 17:43:14 +0100

    Moved to different folder

--- docker/osrm-services/preprocess.bat
+++ docker/osrm-services/preprocess.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-SET PWD=C:\dev\web\agent-smith\packages\osrm-services\data
+SET PWD=C:\dev\web\agent-smith\docker\osrm-services\data
 SET FILE=netherlands-latest
 echo Processing driving data
 
